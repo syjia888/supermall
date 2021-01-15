7590abf595d78ed51ba159dafd1f5246a026ee7b
jiasunyang <15068185743@100fen.cn>
2021-01-15 14:39:29 +0800

    geng

---
+++ src/components/common/swiper/swiper.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="hy-swiper">
+    <div class="swiper">
+      <slot></slot>
+    </div>
+    <slot name="indicator"></slot>
+    <div class="indicator">
+      <slot name="indicator">
+        <div></div>
+      </slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Swiper",
+};
+</script>
+
+<style scoped>
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,35 @@
 <template>
-  <div>
-    <h2>主页</h2>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
   </div>
 </template>
 
 <script>
+import NavBar from "../../components/common/navbar/NavBar";
+import { getHomeMultidata } from "../../network/home";
 export default {
   name: "Home",
+  components: {
+    NavBar,
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+    };
+  },
+  created() {
+    console.log(getHomeMultidata);
+    getHomeMultidata().then((res) => {
+      this.banners = res.data.banner;
+      this.recommends = res.data.recommend.list;
+    });
+  },
 };
 </script>
 <style scoped>
+.home-nav {
+  background-color: pink;
+  color: aliceblue;
+}
 </style>
